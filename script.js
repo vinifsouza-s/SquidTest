1c45ccb6977ad666262fe2a0f69f7887d036f6dd
Vinícius Souza <vinicius.sfs91@gmail.com>
2021-09-25 18:14:22 -0300

    Ajuste do logo e composição dos usuários em tela

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+const users = document.querySelector('.row');
+
 async function infoapi() {
     const url = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic');
     const res =  await url.json();
     console.log(res)
+
+
+    res.forEach((contents) => {
+        const { imagens: { thumbnail: { url } } } = contents;
+            let img = document.createElement('img');
+            let newContent = document.createElement('div');
+            newContent.className = "col";
+            newContent.className = "box";
+            img.className = "img-fluid";
+            img.src = url;
+            newContent.appendChild(img);
+        users.appendChild(newContent);
+      })
 }
 
+
 infoapi();
+
   

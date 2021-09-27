c0c4de396d8582f3f09e7b036bbcfdf1a5ac34be
Vinícius Souza <vinicius.sfs91@gmail.com>
2021-09-26 21:13:16 -0300

    Inclusão dos dados de usuário em tela e link para pagina do Instagram

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 const users = document.querySelector('.row');
 
-
-async function infoapi() {
-    const url = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic');
-    const res =  await url.json();
-    console.log(res);
-
-
-res.forEach((contents) => {
-    const { imagens: { thumbnail: { url } } } = contents;
-        let img = document.createElement('img');
-        let newContent = document.createElement('div');
-        newContent.className = "col";
-        newContent.className = "box";
-        img.className = "img-fluid";
-        img.src = url;
-        newContent.appendChild(img);
-        users.appendChild(newContent);
-      })
-
-}
-
 window.addEventListener("load", function () {
     const loader = document.createElement('div');
         loader.innerHTML = 
@@ -33,9 +12,42 @@ window.addEventListener("load", function () {
         loader.className += " hidden";
 });
 
+function addDate(data) {
+    const [date, time] = new Date(data).toLocaleString().split(' ');
+    const [hour, minute] = time.split(':');
+    return `${date} ${hour}:${minute}`;
+  };
 
-document.addEventListener("DOMContentLoaded", function() {
-    infoapi();
-});
+async function infoapi() {
+    const url = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic');
+    const res =  await url.json();
+    console.log(res);
+
+
+res.forEach((contents) => {
+
+    const {  comentarios, criadoEm, link, 
+        metadados: { engagement }, 
+        imagens: { thumbnail: { url } }, 
+        usuario: { username } } = contents; 
+
+    const newContent = document.createElement('div');
+        newContent.className = 
+        "box";
+        newContent.innerHTML = 
+        `<a href=${link} target="_blank">
+        <img src=${url} class="img-fluid image" />
+        <div class="middle">
+        <p>@${username}<p/>
+        <p><i class="fas fa-comment">  ${comentarios}</i><p/>
+        <p><i class="fas fa-heart">  ${engagement}</i></p>
+        <p><i class="fas fa-calendar-alt">  ${addDate(criadoEm)}</i></p>
+        </div></a>`;
+
+        users.appendChild(newContent);
+      });
+}
+
+document.addEventListener("DOMContentLoaded", function(){infoapi()});
 
 
